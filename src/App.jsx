bc4343c20edc4d64d7f5265aa8cad46f5b8eb052
Umar71-Developer <141718806+Umar71-Developer@users.noreply.github.com>
2025-04-18 22:11:32 +0500

    word matching authentication is applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import  { useState } from 'react'
 import { languages } from "./languages"
+import clsx from 'clsx';
  
 function AssemblyEndGame() {
   const [currentWord , setCurrentWord] = useState("react")
+  const [guessedWord , setGuessedWord] = useState([])
 
-console.log(currentWord)
+  console.log(guessedWord)
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+   function addToKeyboard(letter) {
+      setGuessedWord(prevLetter => {
+       return prevLetter.includes(letter) ? prevLetter : [...prevLetter , letter]
+      }
+      )
+   }
    const word = currentWord.split("").map((letter,index) => {
+      
       return(
       <span key={index} className='letter'>{letter.toUpperCase()}</span>
       )
    })
   
+
+   const keyboardElements = alphabet.split("").map((letter , index) => {
+    const isGuessed = guessedWord.includes(letter)
+      const iscorrect = isGuessed && currentWord.includes(letter);
+      const isWrong = isGuessed && !currentWord.includes(letter)
+      const className = clsx({
+        correct : iscorrect,
+        wrong : isWrong
+      })
+      return(
+        <button onClick={() => addToKeyboard(letter)} key={index} className={className}>{letter.toUpperCase()}</button>
+      )
+   })
 
   const languageElements = languages.map(lang => {
     const styles = {
@@ -35,6 +60,8 @@ console.log(currentWord)
       {languageElements}
       </section>
       <section className='whole-letter'>{word}</section>
+      <section className='keyboard'>{keyboardElements}</section>
+      <button className="new-game">New Game</button>
     </main>
   )
 }
