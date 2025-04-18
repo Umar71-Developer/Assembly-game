a5690d4302c40df8a87c1c380ef7c5ddbcdfc6f4
Umar Hayyat <umarhayyat71.com>
2025-04-18 13:23:04 +0500

    2 section are completed till now

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-
+import  { useState } from 'react'
+import { languages } from "./languages"
+ 
 function AssemblyEndGame() {
+  const [currentWord , setCurrentWord] = useState("react")
+
+console.log(currentWord)
+   const word = currentWord.split("").map((letter,index) => {
+      return(
+      <span key={index} className='letter'>{letter.toUpperCase()}</span>
+      )
+   })
+  
+
+  const languageElements = languages.map(lang => {
+    const styles = {
+      backgroundColor : lang.backgroundColor,
+      color : lang.color
+    }
+    return(
+      <span key={lang.name} className='chip' style={styles}>{lang.name}</span>
+    )
+  })
   return (
     <main>
       <header>
       <h1>Assembly Endgame</h1>
-      <p>Guess the word in under 8 attempts to keep the <br /> programming world safe from Assembly!</p>
+      <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
       <section className="winner-text">
         <h2>You win! </h2>
         <p>Well done! 🎉</p>
       </section>
+      <section className='language-chips'>
+      {languageElements}
+      </section>
+      <section className='whole-letter'>{word}</section>
     </main>
   )
 }
