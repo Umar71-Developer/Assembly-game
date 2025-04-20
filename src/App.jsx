47f6d07a3fdd72850689fa19e1bcb92766248113
Umar Hayyat <umarhayyat71.com>
2025-04-20 16:24:00 +0500

    game status is added and many logics are applied

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import { languages } from "./languages"
 import clsx from 'clsx';
  
 function AssemblyEndGame() {
+  //state values
   const [currentWord , setCurrentWord] = useState("react")
   const [guessedWord , setGuessedWord] = useState([])
 
-  console.log(guessedWord)
-
+  
+  // static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  const wrongguessedLetters = guessedWord.filter((letter) => !currentWord.includes(letter)).length
+  console.log(wrongguessedLetters)
+
+  const isGameWon = currentWord.split("").every((letter) => guessedWord.includes(letter));
+  const isGameLost = wrongguessedLetters >= languages.length -1 ;
+  const isGameOver = isGameWon || isGameLost;
 
    function addToKeyboard(letter) {
       setGuessedWord(prevLetter => {
@@ -19,7 +27,7 @@ function AssemblyEndGame() {
    const word = currentWord.split("").map((letter,index) => {
       
       return(
-      <span key={index} className='letter'>{letter.toUpperCase()}</span>
+      <span key={index} className='letter'>{guessedWord.includes(letter) ?letter.toUpperCase() : ""}</span>
       )
    })
   
@@ -37,31 +45,61 @@ function AssemblyEndGame() {
       )
    })
 
-  const languageElements = languages.map(lang => {
+  const languageElements = languages.map((lang , index) => {
+    const isWrongGuess = index < wrongguessedLetters
     const styles = {
       backgroundColor : lang.backgroundColor,
       color : lang.color
     }
+    
+    // const classNam = clsx("chip", isWrongGuess && "lost")
     return(
-      <span key={lang.name} className='chip' style={styles}>{lang.name}</span>
+      <span key={lang.name} className={`chip ${isWrongGuess ? "lost" : ""}`} style={styles}>{lang.name}</span>
     )
   })
+
+  const gameStatusClass = clsx("winner-text",
+    {won : isGameWon,
+    loose : isGameLost}
+  )
+
+  function renderGameStatus() {
+    if (!isGameOver) {
+        return null
+    }
+
+    if (isGameWon) {
+        return (
+            <>
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h2>Game over!</h2>
+                <p>You lose! Better start learning Assembly 😭</p>
+            </>
+        )
+    }
+}
+
   return (
     <main>
       <header>
       <h1>Assembly Endgame</h1>
       <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className="winner-text">
-        <h2>You win! </h2>
-        <p>Well done! 🎉</p>
-      </section>
+      <section className={gameStatusClass}>
+                {renderGameStatus()}
+            </section>
       <section className='language-chips'>
       {languageElements}
       </section>
       <section className='whole-letter'>{word}</section>
       <section className='keyboard'>{keyboardElements}</section>
-      <button className="new-game">New Game</button>
+      {isGameOver && <button className="new-game">New Game</button>}
     </main>
   )
 }
